e5191d31f601edf9fe938102babc5e4b98397e7e
andela-oowonikoko <oluwaseun.owonikoko@andela.com>
2017-05-16 09:36:47 +0100

    feature 145437697: add file (webpack configuration added)

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import path from 'path';
 import express from 'express';
 import parser from 'body-parser';
 import firebase from 'firebase';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './webpack.config';
 
 const route = require('./server/routes');
 const dotenv = require('dotenv');
@@ -22,10 +26,23 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'test') {
+  const compiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
+app.get('/app/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/dist/index.html`);
+});
+
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
 
 app.use('/', route.todoRouter);
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import firebase from 'firebase';
 
 const todoRouter = express.Router();
 
@@ -6,5 +7,34 @@ todoRouter.route('/')
   .get((req, res) => {
     res.status(200).send("{ message: 'Welcome to Worklist API' }");
   });
+
+todoRouter.get('/login', (req, res) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth()
+  .signInWithPopup(provider).then((result) => {
+    const token = result.credential.accessToken;
+    const user = result.user;
+
+    console.log('token', token);
+    console.log('user', user);
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    console.log('error code', errorCode);
+    console.log('error message', errorMessage);
+  });
+});
+
+todoRouter.get('/signout', (req, res) => {
+  firebase.auth().signOut()
+
+   .then(() => {
+     console.log('Signout Succesfull');
+   }, (error) => {
+     console.log('Signout Failed');
+   });
+});
 
 export default todoRouter;
